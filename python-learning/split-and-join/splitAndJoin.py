795ee60153b199079b8a5fa2fcbe5311f8e983cb
Sivaraman_React_Js <132759927+sivaramankanagavel@users.noreply.github.com>
2023-08-30 13:53:07 +0530

    The Split and join method exercise was done

---
+++ python-learning/split-and-join/splitAndJoin.py
@@ -0,0 +1,13 @@
+"""
+Split and Join method exercise
+"""
+
+# Split
+str_one = "adc,def,ghi,jkl,mno"
+str_split_list = str_one.split(",")
+print(str_split_list)
+
+# Join
+str_two = "Hello World !"
+str_join = "-".join(str_two)
+print(str_join)
