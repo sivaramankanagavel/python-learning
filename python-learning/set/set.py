3cde5b701c272c013b934c4058d59107bae0c003
Sivaraman_React_Js <132759927+sivaramankanagavel@users.noreply.github.com>
2023-08-31 16:55:43 +0530

    The set types exercise was done

---
+++ python-learning/set/set.py
@@ -0,0 +1,17 @@
+"""
+set this is used to store the data like dictionary but without key value.
+
+and it will return the unique values and remove the duplicated values as well as the non-ordered.
+
+you can convert the set into tuples and list.
+"""
+
+# Set exercise:
+
+set_value = {"Dubai", "Qatar", "Saudi", "USA"}
+print(set(set_value))
+
+# converting the set into tuple and list:
+
+print(list(set_value))
+print(tuple(set_value))
