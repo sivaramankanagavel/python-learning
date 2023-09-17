e8ff9d6254fcb554cc0b55d29d7baafee7cca3c4
Sivaraman_React_Js <132759927+sivaramankanagavel@users.noreply.github.com>
2023-09-17 15:14:33 +0530

    The walrus-operator exercise was done

---
+++ Advanced-topics/walrus-operator/walrus-operator.py
@@ -0,0 +1,15 @@
+"""
+Walrus operator is used to short-hand the lengthy code.
+
+Example: name = "Siva"
+         print(name)
+
+         Ans:
+         print(name:="Siva")
+
+The syntax for walrus operator is ':='.
+"""
+name = "Siva"
+print(name)
+
+print(name := "Siva")
