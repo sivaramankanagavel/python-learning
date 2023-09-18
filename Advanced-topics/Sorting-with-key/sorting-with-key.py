6790eb28a8f20a9cea03de69b4e8bf6e52b5423f
Sivaraman_React_Js <132759927+sivaramankanagavel@users.noreply.github.com>
2023-09-18 16:44:48 +0530

    The sort with the dic-of-dict and tuple-of-tuple sort with key feature was done

--- Advanced-topics/Sorting-with-key/sorting-with-key.py
+++ Advanced-topics/Sorting-with-key/sorting-with-key.py
@@ -7,8 +7,13 @@ sorting with key.
 list_tuple_values = [(123, "Hello", True), (345, "world", False), (234, "python", True)]
 list_dict_values = [{"name": "siva", "age": 27}, {"name": "venki", "age": 25},
                     {"name": "Jothiram", "age": 27}]
+tuple_of_tuple = ((4, "Samsung", 25000), (3, "Apple", 45000), (6, "Nokia", 15000))
+dict_of_dict = {"item1": {"name": "siva", "age": 27}, "item2": {"name": "venki", "age": 25},
+                "item3": {"name": "Jothiram", "age": 27}}
 
 list_tuple_values.sort(key=lambda item: item[1])
 print(list_tuple_values)
 list_dict_values.sort(key=lambda item: item["name"], reverse=True)
 print(list_dict_values)
+print(sorted(tuple_of_tuple, key=lambda item: item[1]))
+print(sorted(dict_of_dict.items(), key=lambda item: item[1]["name"]))
