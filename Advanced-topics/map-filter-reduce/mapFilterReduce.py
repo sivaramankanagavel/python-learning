9ab01c817c957c61dcffced9a8dc49c57f5e513a
Sivaraman_React_Js <132759927+sivaramankanagavel@users.noreply.github.com>
2023-09-20 22:37:21 +0530

    The map method exercise was done

---
+++ Advanced-topics/map-filter-reduce/mapFilterReduce.py
@@ -0,0 +1,26 @@
+"""
+Map --> Map is using to iterate each element from the List, here you have to pass the first parameter as
+        function which is used to what we are going to do with iterable item from the map and second
+        parameter as which List you want to iterate, map(function, []).
+"""
+# Question 1:
+list_map_1 = [("Maths", "Anitha", 180), ("Biology", "Anand", 182), ("Biology", "Balaji", 170), ("Maths", "Chandru", 190)]
+
+# Question 2:
+list_map_2 = [72, 79, 99, 12, 56]
+
+# Question 3:
+list_map_3 = [102, 99, 89, 70, 103]
+
+# Ans 1:
+answer_one = list(map(lambda item: (item[1], item[2] / 2), list_map_1))
+
+# Ans 2:
+answer_two = list(map(lambda item: "Even" if item % 2 == 0 else "Odd", list_map_2))
+
+# Ans 3:
+answer_three = list(map(lambda item: float("{:.2f}".format((item - 32) * (5/9))), list_map_3))
+
+print(answer_one)
+print(answer_two)
+print(answer_three)
