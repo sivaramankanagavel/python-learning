16302993f99130fdd40224dec9db04a47ea1377e
Sivaraman_React_Js <132759927+sivaramankanagavel@users.noreply.github.com>
2023-10-27 20:40:32 +0530

    How python interpreter was working

---
+++ Advanced-topics/how-python-interpretor-works/howPythonInterpretorWorks.py
@@ -0,0 +1,5 @@
+"""
+In python each line was immediately converted into byte code by the python interpreter compiler and
+it was goes to the python virtual machine(PVM) the PVM was converted into the executable machine language
+source code --> Python Interpreter -> compiler -> byte code -> PVM -> processor.
+"""
