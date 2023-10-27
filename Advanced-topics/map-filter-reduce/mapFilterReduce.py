364f0a3af7a1811770f39517925bbaaa9e85a2b0
Sivaraman_React_Js <132759927+sivaramankanagavel@users.noreply.github.com>
2023-10-27 13:14:52 +0530

    The map, filter and reduce method exercise was done

--- Advanced-topics/map-filter-reduce/mapFilterReduce.py
+++ Advanced-topics/map-filter-reduce/mapFilterReduce.py
@@ -1,3 +1,4 @@
+from functools import reduce
 """
 Map --> Map is using to iterate each element from the List, here you have to pass the first parameter as
         function which is used to what we are going to do with iterable item from the map and second
@@ -24,3 +25,21 @@ answer_three = list(map(lambda item: float("{:.2f}".format((item - 32) * (5/9)))
 print(answer_one)
 print(answer_two)
 print(answer_three)
+
+"""
+Filter ---> In filter function we need to pass the function as first parameter and 
+            this function have some condition if it satisfied then it will return the
+            data or else return nothing.
+"""
+
+filter_result = list(filter(lambda x: x % 2 == 0, list_map_3))
+print(filter_result)
+
+"""
+Reduce ---> In reduce function we need to pass the first parameter as a function here 
+            we have two arguments received and we process the two arguments and return
+            new list with single data.
+"""
+
+reduce_result = reduce(lambda x, y: x+y, list_map_3)
+print(reduce_result)
